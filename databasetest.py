938054dd2e20cdabc47905803739a20dae791cb4
David <40863988+davidbujic@users.noreply.github.com>
2018-07-27 19:53:28 +0200

    Update databasetest.py

--- databasetest.py
+++ databasetest.py
@@ -1,23 +1,16 @@
 import sqlite3
-
-def Main():
-    try:
-            con = sqlite3.connect('test.db')
-            cur = con.cursor()
-            cur.execute('CREATE TABLE Korisnici(ID INT, Name TEXT, Surname TEXT, Gender TEXT)')
-            cur.execute("INSERT INTO Korisnici VALUES (1, 'David', 'Bujic', 'M')")
-            cur.execute("INSERT INTO Korisnici VALUES (2, 'Zeljko', 'Bujic', 'M')")
-            cur.execute("INSERT INTO Korisnici VALUES (3, 'Lidija', 'Bujic', 'F')")
-            con.commit()
-            cur.execute("SELECT * FROM Korisnici")
-            data = cur.fetchall()
-            for row in data:
-                print row
-    except sqlite3.Error, e:
-            if con:
-                con.rollback()
-                print "Problem sa SQL."
-    finally:
-            if con:
-                con.close()
-Main()
+conn = sqlite3.connect('databasetest.db')
+c = conn.cursor()
+for row in c.execute('SELECT * FROM korisnici'):
+    print row
+ime = raw_input("Unesite Vase ime: ")
+prezime = raw_input("Unesite Vase prezime: ")
+pol = raw_input("Unesite Vas pol:  M za muski pol, F za zenski pol.")
+starost = raw_input("Unesite Vas broj godina: ")
+sql = ("INSERT INTO korisnici VALUES('%s', '%s', '%s', '%d')") % \
+                  (ime, prezime, pol, int(starost))
+number_of_rows = c.execute(sql)
+conn.commit()
+for row in c.execute('SELECT * FROM korisnici'):
+    print row
+conn.close()
