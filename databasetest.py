b89b3eba273289de3d6aa016b24719344b741e5e
David <40863988+davidbujic@users.noreply.github.com>
2018-07-29 13:51:48 +0100

    Update databasetest.py

--- databasetest.py
+++ databasetest.py
@@ -7,8 +7,11 @@ ime = raw_input("Unesite Vase ime: ")
 prezime = raw_input("Unesite Vase prezime: ")
 pol = raw_input("Unesite Vas pol:  M za muski pol, F za zenski pol.")
 starost = raw_input("Unesite Vas broj godina: ")
-sql = ("INSERT INTO korisnici VALUES('%s', '%s', '%s', '%d')") % \
-                  (ime, prezime, pol, int(starost))
+mesto = raw_input("Unesite mesto stanovanja: ")
+opstina = raw_input("Unesite opstinu kojoj pripada Vase mesto stanovanja: ")
+grad = raw_input("Unesite grad kojem pripada Vase mesto stanovanja: ")
+sql = ("INSERT INTO korisnici VALUES('%s', '%s', '%s', '%d', '%s', '%s', '%s')") % \
+                  (ime, prezime, pol, int(starost), mesto, opstina, grad)
 number_of_rows = c.execute(sql)
 conn.commit()
 for row in c.execute('SELECT * FROM korisnici'):
